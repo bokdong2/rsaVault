badbfa12bdf55f13f0d5da5d54992860539e6353
username1565 <39200328+username1565@users.noreply.github.com>
2019-03-15 13:13:51 +0100

    Move this file
    Move this in root directory, and change content.

--- rsaVault/Compile.bat
+++
@@ -1,3 +0,0 @@
-    C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\csc.exe -target:winexe -optimize -out:rsaVault.exe *.cs
-::    C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Release "rsaVault.csproj"
-
